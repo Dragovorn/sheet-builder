cfdcfd0bd02afaf8745a8d92415dbb80cda31065
Andrew Burr <me@dragovorn.com>
2019-03-23 09:56:32 -0400

    Create hello world endpoint to test

---
+++ hello-world/src/main/java/net/sheetbuilder/helloworld/HelloWorldLambdaHandler.java
@@ -0,0 +1,16 @@
+package net.sheetbuilder.helloworld;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloWorldLambdaHandler implements RequestHandler<Map<String, String>, Map<String, String>> {
+
+    public Map<String, String> handleRequest(Map<String, String> stringStringMap, Context context) {
+        return new HashMap<String, String>() {{
+            put("hello", "world");
+        }};
+    }
+}
